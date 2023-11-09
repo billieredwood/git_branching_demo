005a2bac75df405fa2cb797536e54d90177cac37
Billie Redwood <billieredwood@gmail.com>
2023-11-09 11:52:46 +0000

    edit line 6 was added on the different feature branch

--- git_branching_demo/src/main/java/Main.java
+++ git_branching_demo/src/main/java/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("We're writing this on develop!");
+        System.out.println("Editing line 6 on a different branch");
     }
 
 }
